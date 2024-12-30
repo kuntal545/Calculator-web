dd3268cc4d2d16d1ca63c82f50f407ebd9787aaf
Webcraftkuntal <131690513+kuntal545@users.noreply.github.com>
2024-12-30 21:26:42 +0530

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+function appendValue(value) {
+   const resultField = document.getElementById('result');
+   resultField.value += value;
+ }
+ 
+ function clearResult() {
+   const resultField = document.getElementById('result');
+   resultField.value = '';
+ }
+ 
+ function calculateResult() {
+   const resultField = document.getElementById('result');
+   try {
+     resultField.value = eval(resultField.value);
+   } catch (error) {
+     resultField.value = 'Error';
+   }
+ }
